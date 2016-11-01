c3154c75790b0db296f5a815a1abd779e9986726
Alexander Pajer <alexander.pajer@integrations.at>
2016-11-01 10:22:37 +0100

    Advanced JS added & checked

---
+++ 04 Advanced JavaScript/VouchersAdvJavaScript/src/VouchersJQuery/wwwroot/demos/require/credits.js
@@ -0,0 +1,16 @@
+﻿//function getCredits() {
+//    console.log("Function : getCredits");
+//    var credits = "100";
+//    return credits;
+//}
+
+define(function () {
+    console.log("Function : getCredits");
+
+    return {
+        getCredits: function () {
+            var credits = "100";
+            return credits;
+        }
+    }
+});
