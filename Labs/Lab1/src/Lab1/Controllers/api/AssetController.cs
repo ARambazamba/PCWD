b6f32d41adfbe4364cd458894411c67c619972e9
Administrator <Administrator@SP2016>
2017-03-01 08:44:46 +0100

    Labs added

---
+++ Labs/Lab1/src/Lab1/Controllers/api/AssetController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab1.Model;
+
+namespace Lab1.Controllers
+{
+    [Route("api/[controller]")]
+    public class AssetController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public List<Asset> Get()
+        {
+            List<Asset> result = new List<Asset>();
+            result.Add(new Asset() {ID = 1, Name = "Test Asset", Value=10, StartDate = DateTime.Now, Periodes = 5 });
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Labs/Lab1/src/Lab1/Model/Asset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Model
+{
+    public class Asset
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int Periodes { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
