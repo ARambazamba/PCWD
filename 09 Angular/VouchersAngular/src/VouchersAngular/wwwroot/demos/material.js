be8106a40cf470b74f5e916a666b64a5e01f51ba
Alexander Pajer <alexander.pajer@integrations.at>
2017-03-08 06:52:31 +0100

    webpack files added

---
+++ 09 Angular/VouchersAngular/src/VouchersAngular/wwwroot/demos/material.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("app", ["ngMaterial"]);
+
+app.controller("baseCtrl", function ($scope) {
+
+    $http.get("/api/vouchers").then(function (response) {
+        $scope.vouchers = response.data;
+    });
+
+});
