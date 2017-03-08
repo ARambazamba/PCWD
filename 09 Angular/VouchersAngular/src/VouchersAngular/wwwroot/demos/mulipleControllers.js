bb9fa68dd7e393a029fab9a8d7f75e092afe1247
Alexander Pajer <alexander.pajer@integrations.at>
2017-03-08 09:59:09 +0100

    minor fix

--- 09 Angular/VouchersAngular/src/VouchersAngular/wwwroot/demos/mulipleControllers.js
+++ 09 Angular/VouchersAngular/src/VouchersAngular/wwwroot/demos/mulipleControllers.js
@@ -3,16 +3,10 @@ app.value("AppTitle", "Voucher Editor");
 
 app.controller('topController', ['$scope', function ($scope) {
         $scope.Msg = "The top Controller";
-        $scope.toggleCustom = function () {
-            $scope.custom = $scope.custom === false ? true : false;
-        };
 }]);
 
 app.controller('bottomController', ['$scope', function ($scope) {
     $scope.Msg = "The bottm Controller";
-    $scope.toggleCustom = function () {
-        $scope.custom = $scope.custom === false ? true : false;
-    };
 }]);
 
 app.controller('appController', ['$scope', 'AppTitle', function ($scope, title) {
