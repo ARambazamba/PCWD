fd0d1869db18fd34acc88af637e5506f5eb5a18c
Alexander Pajer <alexander.pajer@integrations.at>
2017-06-29 14:54:29 +0200

    Typescript migrated

--- 06 TypeScript/VouchersTypeScript/src/VouchersTypeScript/gulpfile.js
+++ 06 TypeScript/VouchersTypeScript/src/VouchersTypeScript/gulpfile.js
@@ -3,41 +3,57 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var traceur = require('gulp-traceur');
 var babel = require('gulp-babel');
-var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 var typescript = require('gulp-tsc');
 
 var paths = {
-    webroot: "./wwwroot/"
+    webroot: "./wwwroot/",
+    scriptSource: "./wwwroot/demos/*.js",
+    scriptDest: "./wwwroot/js/",
+    demos: "./wwwroot/demos/",
+    scss: "./wwwroot/sass/**/*.scss",
+    scssDest: "./wwwroot/css/"
 }
 
-paths.dataRequest = "./Scripts/*";
-paths.jsDest = paths.webroot + "js/";
-es6Path = 'es6/*.js';
-compilePath = 'es6/compiled';
-
 gulp.task('min:js', function () {
-    return gulp.src([paths.dataRequest])
-        .pipe(concat(paths.jsDest + "min/site.min.js"))
+    return gulp.src([paths.scriptSource])
+        .pipe(concat(paths.scriptDest + "min/site.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
 gulp.task('copy:js', function () {
-    return gulp.src([paths.dataRequest])
-        .pipe(gulp.dest(paths.jsDest));
-});
-
-gulp.task('babel', function () {
-    gulp.src([es6Path])
-        .pipe(plumber())
-        .pipe(babel())
-        .pipe(gulp.dest(compilePath + '/babel'));
+    return gulp.src([paths.scriptSource])
+        .pipe(gulp.dest(paths.scriptDest));
 });
 
 gulp.task("both:tasks", ["min:js", "copy:js"]);
 
-gulp.task('compilets', function(){
-  gulp.src(['wwwroot/**/*.ts'])
-    .pipe(typescript())
-    .pipe(gulp.dest('dest/'))
+gulp.task('compile:sass', function () {
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.scssDest));
+});
+
+gulp.task('watch:sass', function () {
+    gulp.watch(paths.scss, ['compile:sass']);
+});
+
+gulp.task('babel2es5', () => {
+    return gulp.src([paths.demos + "es6*.js"])
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('es5bundle.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.demos));
+});
+
+gulp.task('compile-ts', function() {
+    gulp.src(['wwwroot/**/*.ts'])
+        .pipe(typescript())
+        .pipe(gulp.dest('dest/'));
 });
